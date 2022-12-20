023a7d00567cd04a7df8880711985a520941b2ff
Marie Cari <mcari@nyc.yearup.org>
2022-12-19 19:22:05 -0500

    created method to give user ID

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@ public class Main {
     public static void main(String[] args) {
         User num1 = new User();
 
-        System.out.println(num1.createUserID());
 
     }
 }
--- User.java
+++ User.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class User {
 
     //declare variables
@@ -12,12 +13,27 @@ public class User {
         Scanner scan = new Scanner(System.in);
         this.fullName = scan.nextLine();
 
+        if (fullName.length() <=2) {
+            throw  new RuntimeException("Invalid Name (must be more than 2 letters)");
+        }
 
+        this.userID = createUserID();
+
+        System.out.println("Hello "+ fullName.toUpperCase() + ".\nYour user ID is: " + userID);
 
     }
 
     public String createUserID() {
-        userID = fullName;
+        int length = 3;
+        String numberSet = "1234567890";
+        char[] id = new char[3];
+        for (int i = 0; i < 3; i++) {
+            int random = (int) (Math.random() * numberSet.length());
+            id[i] = numberSet.charAt(random);
+        }
+
+
+        userID = fullName.substring(0,2).toUpperCase() + String.valueOf(id);
         return userID;
     }
 
